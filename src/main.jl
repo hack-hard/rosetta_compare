c34574eea5e9306d0ebf3802d86d131c1fa62d18
tristan hacquard <tristan.hacquard@polytechnique.edu>
2023-10-20 14:52:20 +0200

    added biopython

--- src/main.jl
+++ src/main.jl
@@ -1,6 +1,10 @@
-using DataFrames
-using CSV
-trueData = "/home/hacquard/fullseqdesign_rosetta/pdb.full"
-function loadpdb(file::String)::DataFrame
-    DataFrame(CSV.File(file; header = [:atom, :id, :element,:radical,:chain,:radical_id,:x,:y,:z,:occupancy,:temperature]))
-end
+using BioStructures
+path = "/home/hacquard/fullseqdesign_rosetta/allpositions/rosetta/1ABO.rosetta_0001.pdb"
+truePath = "/home/hacquard/fullseqdesign_rosetta/pdb.full/1ABO.pdb"
+simulated = read(path, PDB)
+real =read(truePath,PDB)
+
+prop(x::AbstractVector{Bool}) = count(x)/length(x)
+
+prop(resname.(simulated["A"]) .== resname.(real["A"]))
+
---
+++ src/moduele.jl
@@ -0,0 +1,6 @@
+module m
+export f
+
+f(x::Integer)= 1
+g(x)= f(x) +1
+end
